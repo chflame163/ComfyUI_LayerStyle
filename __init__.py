5c3de8d4cd3900e301cc2cf6034433b723d4ed9e
chflame <chflame@163.com>
2024-02-26 17:54:20 +0800

    fix web extension files

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ python = sys.executable
 extentions_folder = os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)),
                                  "web" + os.sep + "extensions" + os.sep + "dzNodes")
 javascript_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "js")
-outdate_file_list = ['comfy_shared.js', 'debug.js', 'mtb_widgets.js', 'parse-css.js']
+outdate_file_list = ['comfy_shared.js', 'debug.js', 'mtb_widgets.js', 'parse-css.js', 'dz_widgets.js']
 
 if not os.path.exists(extentions_folder):
     print('# 😺dzNodes: Making the "web\extensions\dzNodes" folder')
