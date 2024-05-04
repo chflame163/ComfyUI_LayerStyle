074a0e9ec7c03fe0df22221f311496c3f12a3cee
chflame <chflame@163.com>
2024-05-04 23:11:35 +0800

    commit ColorTemperature and ColorBalance nodes

---
+++ py/color_correct_color_balance.py
@@ -0,0 +1,74 @@
+from .imagefunc import *
+
+
+NODE_NAME = 'ColorBalance'
+
+class ColorBalance:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(self):
+
+        return {
+            "required": {
+                "image": ("IMAGE", ),  #
+                "cyan_red": ("FLOAT", {"default": 0, "min": -1.0, "max": 1.0, "step": 0.001}),
+                "magenta_green": ("FLOAT", {"default": 0, "min": -1.0, "max": 1.0, "step": 0.001}),
+                "yellow_blue": ("FLOAT", {"default": 0, "min": -1.0, "max": 1.0, "step": 0.001})
+            },
+            "optional": {
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = 'color_balance'
+    CATEGORY = '😺dzNodes/LayerColor'
+
+    def color_balance(self, image, cyan_red, magenta_green, yellow_blue):
+
+        l_images = []
+        l_masks = []
+        ret_images = []
+
+        for l in image:
+            l_images.append(torch.unsqueeze(l, 0))
+            m = tensor2pil(l)
+            if m.mode == 'RGBA':
+                l_masks.append(m.split()[-1])
+            else:
+                l_masks.append(Image.new('L', m.size, 'white'))
+
+
+        for i in range(len(l_images)):
+            _image = l_images[i]
+            _mask = l_masks[i]
+            orig_image = tensor2pil(_image)
+
+            ret_image = color_balance(orig_image,
+                              [cyan_red, magenta_green, yellow_blue],
+                              [cyan_red, magenta_green, yellow_blue],
+                              [cyan_red, magenta_green, yellow_blue],
+                                      shadow_center=0.15,
+                                      midtone_center=0.5,
+                                      midtone_max=1,
+                                      preserve_luminosity=True)
+
+            if orig_image.mode == 'RGBA':
+                ret_image = RGB2RGBA(ret_image, orig_image.split()[-1])
+
+            ret_images.append(pil2tensor(ret_image))
+
+        log(f"{NODE_NAME} Processed {len(ret_images)} image(s).", message_type='finish')
+        return (torch.cat(ret_images, dim=0),)
+
+
+NODE_CLASS_MAPPINGS = {
+    "LayerColor: ColorBalance": ColorBalance
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LayerColor: ColorBalance": "LayerColor: ColorBalance"
+}
---
+++ py/color_correct_color_temperature.py
@@ -0,0 +1,57 @@
+# Adapt from https://github.com/EllangoK/ComfyUI-post-processing-nodes/blob/master/post_processing/color_correct.py
+
+from .imagefunc import *
+
+NODE_NAME = 'ColorTemperature'
+
+class ColorTemperature:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "temperature": ("FLOAT", {"default": 0, "min": -100, "max": 100, "step": 1},),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "color_temperature"
+    CATEGORY = '😺dzNodes/LayerColor'
+
+    def color_temperature(self, image, temperature,
+                            ):
+
+        batch_size, height, width, _ = image.shape
+        result = torch.zeros_like(image)
+
+        temperature /= -100
+
+        for b in range(batch_size):
+            tensor_image = image[b].numpy()
+            modified_image = Image.fromarray((tensor_image * 255).astype(np.uint8))
+            modified_image = np.array(modified_image).astype(np.float32)
+
+            if temperature > 0:
+                modified_image[:, :, 0] *= 1 + temperature
+                modified_image[:, :, 1] *= 1 + temperature * 0.4
+            elif temperature < 0:
+                modified_image[:, :, 0] *= 1 + temperature * 0.2
+                modified_image[:, :, 2] *= 1 - temperature
+
+            modified_image = np.clip(modified_image, 0, 255)
+            modified_image = modified_image.astype(np.uint8)
+            modified_image = modified_image / 255
+            modified_image = torch.from_numpy(modified_image).unsqueeze(0)
+            result[b] = modified_image
+
+        log(f"{NODE_NAME} Processed {len(result)} image(s).", message_type='finish')
+        return (result,)
+
+NODE_CLASS_MAPPINGS = {
+    "LayerColor: ColorTemperature": ColorTemperature
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LayerColor: ColorTemperature": "LayerColor: ColorTemperature"
+}
