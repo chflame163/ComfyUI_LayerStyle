6414c85c30acadea42e5589e1cb35dd68401bed4
chflame163 <chflame@163.com>
2024-08-29 09:10:11 +0800

    fix EVF-SAMUltra model name

--- py/evf_sam_ultra.py
+++ py/evf_sam_ultra.py
@@ -5,7 +5,7 @@ import sys
 
 from .imagefunc import *
 sys.path.append(os.path.join(os.path.dirname(__file__), 'evf_sam'))
-from inference import evf_sam_main
+from evf_sam.evf_sam_inference import evf_sam_main
 class EVF_SAM_Ultra:
 
     def __init__(self):
