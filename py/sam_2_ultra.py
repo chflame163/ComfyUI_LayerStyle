2d12cab15300432c43d26dae1966de8eac4a12ae
chflame163 <chflame@163.com>
2024-09-10 21:01:13 +0800

    fix bug of SAM2Ultra node

--- py/sam_2_ultra.py
+++ py/sam_2_ultra.py
@@ -382,10 +382,10 @@ class LS_SAM2_ULTRA:
         detail_range = detail_erode + detail_dilate
         if process_detail:
             if detail_method == 'GuidedFilter':
-                _mask = guided_filter_alpha(i, _mask, detail_range // 6 + 1)
+                _mask = guided_filter_alpha(pil2tensor(orig_image), _mask, detail_range // 6 + 1)
                 _mask = tensor2pil(histogram_remap(_mask, black_point, white_point))
             elif detail_method == 'PyMatting':
-                _mask = tensor2pil(mask_edge_detail(i, _mask, detail_range // 8 + 1, black_point, white_point))
+                _mask = tensor2pil(mask_edge_detail(pil2tensor(orig_image), _mask, detail_range // 8 + 1, black_point, white_point))
             else:
                 _trimap = generate_VITMatte_trimap(_mask, detail_erode, detail_dilate)
                 _mask = generate_VITMatte(orig_image, _trimap, local_files_only=local_files_only, device=device,
