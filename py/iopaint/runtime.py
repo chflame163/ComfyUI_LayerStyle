67c7a930bcf2a07e56f114897f57e7aed71fae60
chflame163 <chflame@163.com>
2024-09-24 00:02:09 +0800

    fix bug of Lama

--- py/iopaint/runtime.py
+++ py/iopaint/runtime.py
@@ -11,35 +11,35 @@ from rich import print
 from typing import Dict, Any
 
 
-_PY_VERSION: str = sys.version.split()[0].rstrip("+")
+# _PY_VERSION: str = sys.version.split()[0].rstrip("+")
 
-if packaging.version.Version(_PY_VERSION) < packaging.version.Version("3.8.0"):
-    import importlib_metadata  # type: ignore
-else:
-    import importlib.metadata as importlib_metadata  # type: ignore
+# if packaging.version.Version(_PY_VERSION) < packaging.version.Version("3.8.0"):
+#     import importlib_metadata  # type: ignore
+# else:
+#     import importlib.metadata as importlib_metadata  # type: ignore
 
-_package_versions = {}
+# _package_versions = {}
 
-_CANDIDATES = [
-    "torch",
-    "torchvision",
-    "Pillow",
-    "diffusers",
-    "transformers",
-    "opencv-python",
-    "accelerate",
-    "iopaint",
-    "rembg",
-    "realesrgan",
-    "gfpgan",
-]
+# _CANDIDATES = [
+#     "torch",
+#     "torchvision",
+#     "Pillow",
+#     "diffusers",
+#     "transformers",
+#     "opencv-python",
+#     "accelerate",
+#     "iopaint",
+#     "rembg",
+#     "realesrgan",
+#     "gfpgan",
+# ]
 # Check once at runtime
-for name in _CANDIDATES:
-    _package_versions[name] = "N/A"
-    try:
-        _package_versions[name] = importlib_metadata.version(name)
-    except importlib_metadata.PackageNotFoundError:
-        pass
+# for name in _CANDIDATES:
+#     _package_versions[name] = "N/A"
+#     try:
+#         _package_versions[name] = importlib_metadata.version(name)
+#     except importlib_metadata.PackageNotFoundError:
+#         pass
 
 
 def dump_environment_info() -> Dict[str, str]:
