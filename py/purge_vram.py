7b326d13e43fc4022cd80e472c7af67027409b1e
chflame163 <chflame@163.com>
2025-02-13 13:07:10 +0800

    commit PurgeVRAM V2 node

--- py/purge_vram.py
+++ py/purge_vram.py
@@ -8,7 +8,39 @@ any = AnyType("*")
 class PurgeVRAM:
 
     def __init__(self):
-        self.NODE_NAME = 'PurgeVRAM'
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "anything": (any, {}),
+                "purge_cache": ("BOOLEAN", {"default": True}),
+                "purge_models": ("BOOLEAN", {"default": True}),
+            },
+            "optional": {
+            }
+        }
+
+    RETURN_TYPES = ()
+    FUNCTION = "purge_vram"
+    CATEGORY = '😺dzNodes/LayerUtility/SystemIO'
+    OUTPUT_NODE = True
+
+    def purge_vram(self, anything, purge_cache, purge_models):
+        import torch.cuda
+        import gc
+        import comfy.model_management
+        clear_memory()
+        if purge_models:
+            comfy.model_management.unload_all_models()
+            comfy.model_management.soft_empty_cache()
+        return (None,)
+
+class PurgeVRAM_V2:
+
+    def __init__(self):
+        self.NODE_NAME = 'PurgeVRAM V2'
 
     @classmethod
     def INPUT_TYPES(cls):
@@ -25,11 +57,11 @@ class PurgeVRAM:
 
     RETURN_TYPES = (any,)
     RETURN_NAMES = ("any",)
-    FUNCTION = "purge_vram"
+    FUNCTION = "purge_vram_v2"
     CATEGORY = '😺dzNodes/LayerUtility/SystemIO'
     OUTPUT_NODE = True
 
-    def purge_vram(self, anything, purge_cache, purge_models):
+    def purge_vram_v2(self, anything, purge_cache, purge_models):
         clear_memory()
         if purge_models:
             comfy.model_management.unload_all_models()
@@ -38,9 +70,11 @@ class PurgeVRAM:
 
 
 NODE_CLASS_MAPPINGS = {
-    "LayerUtility: PurgeVRAM": PurgeVRAM
+    "LayerUtility: PurgeVRAM": PurgeVRAM,
+    "LayerUtility: PurgeVRAM V2": PurgeVRAM_V2,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LayerUtility: PurgeVRAM": "LayerUtility: Purge VRAM"
+    "LayerUtility: PurgeVRAM": "LayerUtility: Purge VRAM",
+    "LayerUtility: PurgeVRAM V2": "LayerUtility: Purge VRAM V2",
 }
