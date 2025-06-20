3bfe8e435d167b4a5bf08716729f89802dbbaa6f
chflame163 <chflame@163.com>
2025-06-20 08:44:58 +0800

    remove LUT Apply node debug info

--- py/color_correct_LUTapply.py
+++ py/color_correct_LUTapply.py
@@ -34,8 +34,7 @@ class ColorCorrectLUTapply:
 
     def color_correct_LUTapply(self, image, LUT, color_space, strength):
 
-        (LUT_DICT, _) = get_resource_dir()
-        log(f"LUT_DICT={LUT_DICT}")
+        (LUT_DICT, _) = get_resource_dir()        
         ret_images = []
         for i in image:
             i = torch.unsqueeze(i, 0)
